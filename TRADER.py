59b0bbff0c7c05621de079d726c561127e20d7e7
Berre97 <bertremon@hotmail.com>
2024-10-20 21:25:04 +0200

    Update TRADER.py

--- TRADER.py
+++ TRADER.py
@@ -51,8 +51,6 @@ class apibot():
             try:
                 with open(file_path, 'r') as f:
                     data = json.load(f)
-                    print(data)
-
                     if not isinstance(data, list):
                         data = []
 
